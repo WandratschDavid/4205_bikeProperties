17a52a98cd435c3a5a5afcf011020b6adba22859
WandratschDavid <72546181+David291202@users.noreply.github.com>
2022-01-20 15:36:32 +0100

    initial commit

---
+++ src/main/TheMain.java
@@ -0,0 +1,31 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TheMain extends Application
+{
+    @Override
+    public void init()
+    {
+        //Catalog.getInstance().restore();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        //BikePropertiesC.show(stage);
+    }
+
+    @Override
+    public void stop() throws Exception
+    {
+        super.stop();
+        //Catalog.getInstance().persist();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
